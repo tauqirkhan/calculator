0a2f8b5dcb8520187af456d8052f127526285613
Tauqir <tauqirkhann.11@gmail.com>
2024-01-19 20:22:53 +0530

    Create add, divide, subtract and divide functiion

---
+++ app.js
@@ -0,0 +1,21 @@
+let firstNum;
+let operator;
+let secondNum;
+
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
