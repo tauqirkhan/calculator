615964f6696693275d55636e275eaafa7794afb9
Tauqir <tauqirkhann.11@gmail.com>
2024-01-20 10:57:40 +0530

    Add working calculator with UI

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let firstNum;
-let operator;
-let secondNum;
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+let result = 0;
 
 
 function add(a, b){
@@ -16,6 +17,80 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if(b == 0){
+        return 'Error';
+    }
     return a / b;
 }
 
+function operate(num1, num2, operator){
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        break;
+        case '-':
+            return subtract(num1, num2);
+        break;
+        case '*':
+            return multiply(num1, num2);
+        break;
+        case '/':
+            return Math.round(divide(num1, num2) * 100)/100;
+        break;
+        default:
+            return 'ERROR';
+    }
+}
+
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.displayinfo');
+
+
+let numValue = '';
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        //number button
+        if(button.classList.contains('number')){
+            numValue = numValue.concat(button.innerText);
+            if(operator == ''){
+                firstNum = numValue;
+                display.innerText = firstNum;
+            }else{
+                secondNum = numValue;
+                display.innerText = secondNum;
+            }
+        }
+        //operator button
+
+        if(button.classList.contains('operator')){
+            operator = button.innerText;
+            numValue = '';
+        }
+        //equal button
+        if(button.classList.contains('equal')){
+            if(!(operator == '')){
+                if(firstNum == ''){
+                    firstNum = 0;
+                }
+                if(secondNum == ''){
+                    secondNum = 0;
+                }
+                result = operate(+firstNum, +secondNum, operator);
+                display.innerText = result;
+                firstNum = result;
+                numValue = '';
+                secondNum = '';
+            }
+        }
+        //clear button
+        if(button.classList.contains('clear')){
+            result = 0;
+            numValue = ''
+            firstNum = '';
+            secondNum = '';
+            operator = '';
+            display.innerText = '0';
+        }
+    });
+});
---
+++ temp.md
@@ -0,0 +1,10 @@
+    //check if number button is clicked and get it's value
+        //if number button clicked store it's value as a number on firstNum until operator is clicked
+    //check if operator button is clicked and get it's value
+        //if operator is clicked store it's value on operator
+            // Then store number button click value on secondNum
+    //check if equal button is clicked
+        //call operate function and get it argument with value firstNum, secondNum and operator and store it value in result and display it on '.displayinfo' div
+            //swap first numValue with result
+    //check if clear button is clicked
+        //set firstNum, secondNum to 0 and operator to ''
